f419556a7f56ccca2321f7186defe07805fc123d
서지헌 <wlgjs8474@gmail.com>
2024-07-19 14:12:11 +0900

    Feat: spring 프로젝트 폴더 생성 및 의존성 추가

---
+++ backend/pitter-patter/src/test/java/com/pitpat/pitter_patter/PitterPatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitpat.pitter_patter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitterPatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
