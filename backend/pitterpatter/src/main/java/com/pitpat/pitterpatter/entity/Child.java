38b3d65cd3dc4aa289a97f44ffc2b15b3f97af10
김상범 <picel@picel.net>
2024-07-22 17:13:28 +0900

    Feat : NotNull 어노테이션 추가 [#116]

--- backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/entity/Child.java
+++ backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/entity/Child.java
@@ -1,14 +1,14 @@
 package com.pitpat.pitterpatter.entity;
 
-import com.pitpat.pitterpatter.entity.Gender;
+import com.pitpat.pitterpatter.entity.ENUM.Gender;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,34 +18,48 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "child")
 public class Child {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "child_id")
+    @NotNull
     private Long id;
+
+    // TODO: user_id FK 필요.
 
     private String profile_image;
 
+    @NotNull
     private String nickname;
 
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
 
     @CreatedDate
+    @NotNull
     private LocalDate created_at;
 
     @LastModifiedDate
+    @NotNull
     private LocalDate updated_at;
 
+    @NotNull
     private LocalDate birth;
 
+    @NotNull
     private int personal_record;
 
+    @NotNull
     private int point;
 
     @OneToMany(fetch = FetchType.LAZY)
+    @NotNull
     private List<ChildItem> childItem = new ArrayList<>();
 
     @OneToMany(mappedBy = "child", fetch = FetchType.LAZY)
-    private List<PointRecord> pointRecords = new ArrayList<>();
+    @NotNull
+    private List<Point> points = new ArrayList<>();
 }
