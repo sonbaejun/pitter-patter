f18ee48ccec7b550a01ccfc57abcac84f999b7d8
이승민 <lsm5631@naver.com>
2024-07-22 14:01:06 +0900

    Chore : 프로젝트 파일 통합 [#138]

---
+++ backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/PitterpatterApplication.java
@@ -0,0 +1,13 @@
+package com.pitpat.pitterpatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitterpatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitterpatterApplication.class, args);
+	}
+
+}
---
+++ backend/pitterpatter/src/test/java/com/pitpat/pitterpatter/PitterpatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitpat.pitterpatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitterpatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
