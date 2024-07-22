8835ba4fabff7f85e36291547fbeb7e90a3afed7
sonbaejun <sonqowns2@naver.com>
2024-07-22 16:48:10 +0900

    Feat : NotNull 제약조건 추가 [#139]

--- backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/entity/Child.java
+++ backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/entity/Child.java
@@ -1,6 +1,8 @@
 package com.pitpat.pitterpatter.entity;
 
+import com.pitpat.pitterpatter.entity.ENUM.Gender;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -16,34 +18,47 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "child")
 public class Child {
 
     @Id @GeneratedValue
     @Column(name = "child_id")
+    @NotNull
     private Long id;
+
+    // TODO: user_id FK 필요.
 
     private String profile_image;
 
+    @NotNull
     private String nickname;
 
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
 
     @CreatedDate
+    @NotNull
     private LocalDate created_at;
 
     @LastModifiedDate
+    @NotNull
     private LocalDate updated_at;
 
+    @NotNull
     private LocalDate birth;
 
+    @NotNull
     private int personal_record;
 
+    @NotNull
     private int point;
 
     @OneToMany(fetch = FetchType.LAZY)
+    @NotNull
     private List<ChildItem> childItem = new ArrayList<>();
 
     @OneToMany(mappedBy = "child", fetch = FetchType.LAZY)
+    @NotNull
     private List<Point> points = new ArrayList<>();
 }
