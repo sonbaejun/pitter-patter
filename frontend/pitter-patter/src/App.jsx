5e2535391b3926b93477a77e0994ea991a9994e9
윤예리 <yoonr721@gmail.com>
2024-07-23 17:30:51 +0900

    Feat : 새 jsx 파일 생성 및 css 구조 생성

--- frontend/pitter-patter/src/App.jsx
+++ frontend/pitter-patter/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import LandingPage from "./pages/Main/LandingPage"
+import NavBar from "./pages/Main/Header"
 
 function App() {
+  
+  // return 에 router 들어갈 거임
   return (
     <div className='App'>
-      <></>
+      <NavBar />
     </div>
   )
 }
