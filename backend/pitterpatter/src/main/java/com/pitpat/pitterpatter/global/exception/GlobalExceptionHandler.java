412abd137a2a066e687b887aef353333e7fa2a7f
sonbaejun <sonqowns2@naver.com>
2024-07-24 11:55:15 +0900

    Feat : Exception 추가 [#146]
    Change-Id: I6bbb495a3efe7d3450b06766d77ca466023edb14

---
+++ backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.pitpat.pitterpatter.global.exception;
+
+import com.pitpat.pitterpatter.global.exception.exceptions.DataNotFoundException;
+import com.pitpat.pitterpatter.global.exception.exceptions.TokenExpiredException;
+import com.pitpat.pitterpatter.global.exception.exceptions.UserProblemException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleDataNotFoundException(DataNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("msg", "해당 데이터가 존재하지 않습니다.");
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<Map<String, String>> handleTokenExpiredException(TokenExpiredException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("msg", "로그인 토큰/세션이 만료되었습니다.");
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(UserProblemException.class)
+    public ResponseEntity<Map<String, String>> handleUserProblemException(UserProblemException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("msg", "잘못된 요청입니다.");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("msg", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // 다른 예외 핸들러도 추가할 수 있습니다.
+}
