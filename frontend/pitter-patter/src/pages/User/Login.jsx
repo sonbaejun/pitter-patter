ea1e69808464ee7ef600d3e9073f15e7852204ff
김상범 <picel@picel.net>
2024-07-24 15:51:44 +0900

    Chore : User 코드 최적화 [#129]
    - Login.jsx 필요 없는 주석 제거
    - Login.css에서 User 공통으로 활용 가능한 부분 User-common.css 파일로 분리
    
    Change-Id: I8a906595c6d710e842f83cb1162e5e3edd573eaa

--- frontend/pitter-patter/src/pages/User/Login.jsx
+++ frontend/pitter-patter/src/pages/User/Login.jsx
@@ -1,5 +1,5 @@
 import "./Login.css";
-// get src/assets/img
+
 import X from "../../assets/img/logo/X.png";
 import kakao from "../../assets/img/logo/kakao.png";
 import naver from "../../assets/img/logo/naver.png"; 
