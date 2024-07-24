b153de23f30d2144f7dd86b3ec1803d94a874b68
sonbaejun <sonqowns2@naver.com>
2024-07-24 15:10:33 +0900

    Feat : 자녀 등록 서비스 기능 개발 [#146]
    Change-Id: I4428e29fee3497107f7682ca639f896b54cad63a

--- backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/domain/child/service/ChildService.java
+++ backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/domain/child/service/ChildService.java
@@ -1,11 +1,20 @@
 package com.pitpat.pitterpatter.domain.child.service;
 
+import com.pitpat.pitterpatter.domain.child.model.dto.ChildRequestDTO;
 import com.pitpat.pitterpatter.domain.child.model.dto.ChildResponseDTO;
 import com.pitpat.pitterpatter.entity.Child;
+import com.pitpat.pitterpatter.entity.UserEntity;
+import com.pitpat.pitterpatter.entity.enums.Gender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface ChildService {
     List<ChildResponseDTO> getChildrenByUserId(Integer userId);
+
+    void addChild(ChildRequestDTO childRequestDTO);
+
+    void childExceptionHandling(List<ChildResponseDTO> children);
+
+    void userExceptionHandling(UserEntity user);
 }
--- backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/domain/child/service/ChildServiceImpl.java
+++ backend/pitterpatter/src/main/java/com/pitpat/pitterpatter/domain/child/service/ChildServiceImpl.java
@@ -1,8 +1,11 @@
 package com.pitpat.pitterpatter.domain.child.service;
 
+import com.pitpat.pitterpatter.domain.child.model.dto.ChildRequestDTO;
 import com.pitpat.pitterpatter.domain.child.model.dto.ChildResponseDTO;
 import com.pitpat.pitterpatter.domain.child.repository.ChildRepository;
 import com.pitpat.pitterpatter.entity.Child;
+import com.pitpat.pitterpatter.entity.UserEntity;
+import com.pitpat.pitterpatter.global.exception.exceptions.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,49 @@ public class ChildServiceImpl implements ChildService {
 
     @Override
     public List<ChildResponseDTO> getChildrenByUserId(Integer userId) {
-        return childRepository.findByUserUserId(userId);
+        List<ChildResponseDTO> childrenByUserId = childRepository.findByUserUserId(userId);
+        childExceptionHandling(childrenByUserId);
+        return childrenByUserId;
+    }
+
+    @Override
+    public void addChild(ChildRequestDTO childRequestDTO) {
+        // TODO : 유저 정보 가져오는 로직 필요
+        UserEntity user = new UserEntity();
+        user.setUserId(1); // 테스트용 데이터
+
+        // TODO : user가 없는 경우에 대한 예외처리 : user도메인 개발 완료 시 추가.(ForeignKeyConstraintException)
+        userExceptionHandling(user);
+
+        Child child = Child.builder()
+                .profileImage(childRequestDTO.getProfileImage())
+                .user(user)
+                .nickname(childRequestDTO.getNickname())
+                .gender(childRequestDTO.getGender())
+                .birth(childRequestDTO.getBirth())
+                .build();
+
+        childRepository.save(child);
+    }
+
+    /**
+     *
+     * Exception 검증 메소드
+     */
+    public void childExceptionHandling(List<ChildResponseDTO> children) {
+        // TODO: 유저 토큰 기능 개발 완료 시 해당 예외처리 체크 필요.
+        // 토큰/세션 만료 등의 검사가 필요할 경우
+        // if (토큰 만료 조건) {
+        //     throw new TokenExpiredException("로그인 토큰/세션이 만료되었습니다.");
+        // }
+
+        if (children.isEmpty()) {
+            throw new DataNotFoundException("해당 데이터가 존재하지 않습니다.");
+        }
+    }
+
+    @Override
+    public void userExceptionHandling(UserEntity user) {
+
     }
 }
