6cbe02796d50c8cbc2986f8907e5f7162b3adde1
김상범 <picel@picel.net>
2024-07-29 16:00:27 +0900

    Feat : 인생네컷 페이지 제작 (in progress)
    Change-Id: I9f4a96c61b6d53e9386ec420901fd02c82c1315f

---
+++ frontend/pitter-patter/src/pages/Game/Snapshot/Snapshot.jsx
@@ -0,0 +1,31 @@
+import "./Snapshot.css";
+
+
+function Snapshot() {
+  return (
+    <div className="center-row">
+      <div className="frame">
+        <div className="blank-row">
+          <div className="blank"></div>
+          <div className="blank"></div>
+        </div>
+        <div className="blank-row">
+          <div className="blank"></div>
+          <div className="blank"></div>
+        </div>
+      </div>
+      <div className="frame">
+        <div className="blank-row">
+          <div className="blank"></div>
+          <div className="blank"></div>
+        </div>
+        <div className="blank-row">
+          <div className="blank"></div>
+          <div className="blank"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Snapshot
