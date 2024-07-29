ec92112b62c4e523f1591654c5b9f6ac0ea127f4
윤예리 <yoonr721@gmail.com>
2024-07-29 14:48:23 +0900

    Feat : 출석 페이지 추가 [#171]
    Change-Id: I9025fd4539714965ba93d64d3ac43257a7049cbf

---
+++ frontend/pitter-patter/src/pages/ETC/Attendance/AttendancePage.jsx
@@ -0,0 +1,12 @@
+import './Attendance.css';
+
+function AttendancePage() {
+    return (
+        <div className="attendance-container">
+            <h1>Attendance Page</h1>
+            <p>Welcome to the Attendance Page!</p>
+        </div>
+    );
+}
+
+export default AttendancePage;
