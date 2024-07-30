164aaa5fce9b21a3fea6cb9929a325eb029c56d9
김상범 <picel@picel.net>
2024-07-30 14:31:25 +0900

    Feat : 게임 페이지 제작 [#232] (1)
    Change-Id: I14a42863528df6dcdb0e4286c5d1e3127795efab

---
+++ frontend/pitter-patter/src/pages/Game/GamePageMulti.jsx
@@ -0,0 +1,12 @@
+import "./GamePageMulti.css";
+
+function GamePageMulti() {
+  return (
+    <div className="main-wrap">
+      <div className="my-game"></div>
+      <div className="opponent-game"></div>
+    </div>
+  )
+}
+
+export default GamePageMulti
