f4995d7d973cadca1f9412f49d482eca05f34ec3
윤예리 <yoonr721@gmail.com>
2024-07-30 14:37:23 +0900

    Feat : 404페이지 추가중 [#198]
    Change-Id: I7c85737475fb95315108e92d9d66784eb4e80f1c

--- frontend/pitter-patter/src/components/ProfileList.jsx
+++ frontend/pitter-patter/src/components/ProfileList.jsx
@@ -1,6 +1,23 @@
+import { Route } from "react-router-dom";
+
 function ProfileList() {
+    const children = ['src/assets/img/User/SingingBanana.png', 'src/assets/img/User/SingingBanana.png', 'src/assets/img/User/SingingBanana.png', 'src/assets/img/User/SingingBanana.png']
+
     return (
-        <div></div>
+        <div>
+            <div className="profile-list">
+
+                {children.map((imgsrc, index) => (
+                    <div className="children-list-img">
+                        <div className="children-profile-item">
+                            <Route to='/'>
+                                <img key={index} src={`${imgsrc}`} alt="profile" className="children-item-img" />
+                            </Route>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
 
---
+++ frontend/pitter-patter/src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+
+
+function Error() {
+    return(
+        <div>
+            <NavBar />
+        </div>
+
+    )
+}
+
+export default Error;
