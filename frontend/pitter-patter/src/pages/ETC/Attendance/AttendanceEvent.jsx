932ad559735e28aec280be5e06cadda3f2d69492
이승민 <lsm5631@naver.com>
2024-07-31 09:17:12 +0900

    Feat : attendance 출석 이벤트 페이지 선긋기 [#184]
    Change-Id: I6de4d7d11201bb77d70bd129e3c02601c7948105

--- frontend/pitter-patter/src/pages/ETC/Attendance/AttendanceEvent.jsx
+++ frontend/pitter-patter/src/pages/ETC/Attendance/AttendanceEvent.jsx
@@ -23,32 +23,35 @@ const stonePositions = [
   { src: UndoneImage, alt: "undone", style: { top: '16%', left: '30%' } },
   { src: UndoneImage, alt: "undone", style: { top: '15%', left: '50%' } },
   { src: UndoneImage, alt: "undone", style: { top: '10%', left: '66%' } },
-  { src: UndoneImage, alt: "undone", style: { top: '30%', left: '78%' } },
+
+  { src: UndoneImage, alt: "undone", style: { top: '30%', left: '81%' } },
   { src: UndoneImage, alt: "undone", style: { top: '37%', left: '50%' } },
-  { src: UndoneImage, alt: "undone", style: { top: '33%', left: '30%' } },
-  { src: UndoneImage, alt: "undone", style: { top: '42%', left: '10%' } },
-  { src: UndoneImage, alt: "undone", style: { top: '50%', left: '30%' } },
-  { src: UndoneImage, alt: "undone", style: { top: '52%', left: '50%' } },
+  { src: UndoneImage, alt: "undone", style: { top: '33%', left: '20%' } },
+  { src: UndoneImage, alt: "undone", style: { top: '42%', left: '5%' } },
+
+  { src: UndoneImage, alt: "undone", style: { top: '50%', left: '31%' } },
+  { src: UndoneImage, alt: "undone", style: { top: '52%', left: '53%' } },
   { src: UndoneImage, alt: "undone", style: { top: '49%', left: '70%' } },
   { src: UndoneImage, alt: "undone", style: { top: '66%', left: '82%' } },
+
   { src: UndoneImage, alt: "undone", style: { top: '70%', left: '45%' } },
   { src: UndoneImage, alt: "undone", style: { top: '80%', left: '23%' } },
 ];
 
 function generatePathData(positions) {
   const pathData = positions.map(pos => {
-    const x = parseFloat(pos.style.left) * 10;
-    const y = parseFloat(pos.style.top) * 10;
+    const x = parseFloat(pos.style.left) + 2;
+    const y = parseFloat(pos.style.top) + 5;
     return { x, y };
   });
 
-  let pathString = `M${pathData[0].x},${pathData[0].y}`;
+  let pathString = `M${pathData[0].x} ${pathData[0].y}`;
   for (let i = 1; i < pathData.length; i++) {
     const prev = pathData[i - 1];
     const current = pathData[i];
     const cx = (prev.x + current.x) / 2;
     const cy = (prev.y + current.y) / 2;
-    pathString += ` Q${cx},${cy} ${current.x},${current.y}`;
+    pathString += ` Q${cx} ${cy} ${current.x} ${current.y}`;
   }
   return pathString;
 }
@@ -69,19 +72,19 @@ function AttendanceEvent() {
           </LayoutColumn>
           <LayoutColumn as={MainWrap}>
             <Board>
-              {stonePositions.map((pos, index) => (
-                <Stone key={index} src={pos.src} alt={pos.alt} style={pos.style} />
-              ))}
               <SVGContainer>
-                <svg width="100%" height="100%">
+                <svg viewBox="0 0 100 100" preserveAspectRatio="none" width="100%" height="100%">
                   <path
                     d={generatePathData(stonePositions)}
-                    stroke="green"
+                    stroke="#629d1b"
                     strokeWidth="2"
                     fill="none"
-                    strokeDasharray="5,5"
-                  />
+                    strokeDasharray="2.5, 2.5"
+                    />
                 </svg>
+                    {stonePositions.map((pos, index) => (
+                      <Stone key={index} src={pos.src} alt={pos.alt} style={pos.style} />
+                    ))}
               </SVGContainer>
             </Board>
           </LayoutColumn>
