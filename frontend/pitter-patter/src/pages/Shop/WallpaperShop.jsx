893ef9f60f89e1fdfb3fca24084dc39dcbe1839b
김상범 <picel@picel.net>
2024-07-31 16:31:13 +0900

    Feat : 상점 페이지 작성 (3/3) [#223]
    - 배경화면 저장
    - 사진 변경, Carousel 구현
    
    Change-Id: I24bb5306cd30acddffccfe755d3144d4ceb4beca

---
+++ frontend/pitter-patter/src/pages/Shop/WallpaperShop.jsx
@@ -0,0 +1,95 @@
+import { ToolBar, Wallpaper, GuideText, CarouselWrap, Preview, TransparentButton, ButtonIcon, RowWrap, ActionButton, PreviewFilter, ActionRow } from "./WallpaperShopStyle";
+
+import { useState, useRef } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+function WallpaperShop() {
+  const Navigator = useNavigate();
+
+  const [wallpapers, setWallpapers] = useState([
+    "/src/assets/img/Shop/wallpaper/background1.png",
+    "/src/assets/img/Shop/wallpaper/background2.png",
+    "/src/assets/img/Shop/wallpaper/background3.png",
+    "/src/assets/img/Shop/wallpaper/background4.png",
+    "/src/assets/img/Shop/wallpaper/background5.png",
+    "/src/assets/img/Shop/wallpaper/background1.png",
+    "/src/assets/img/Shop/wallpaper/background2.png",
+    "/src/assets/img/Shop/wallpaper/background3.png",
+    "/src/assets/img/Shop/wallpaper/background4.png",
+    "/src/assets/img/Shop/wallpaper/background5.png",
+    "/src/assets/img/Shop/wallpaper/background1.png",
+    "/src/assets/img/Shop/wallpaper/background2.png",
+    "/src/assets/img/Shop/wallpaper/background3.png",
+    "/src/assets/img/Shop/wallpaper/background4.png",
+    "/src/assets/img/Shop/wallpaper/background5.png",
+  ]);
+
+  const carouselRef = useRef(null);
+
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const [selectedWallpaper, setSelectedWallpaper] = useState(0);
+
+  const handleLeft = () => {
+    carouselRef.current.scroll({
+      left: carouselRef.current.scrollLeft - window.innerHeight * 0.23,
+      behavior: "smooth"
+    })
+  };
+
+  const handleRight = () => {
+    carouselRef.current.scroll({
+      left: carouselRef.current.scrollLeft + window.innerHeight * 0.23,
+      behavior: "smooth"
+    })
+  };
+
+  const cancel = () => {
+    Navigator(-1);
+  };
+
+  const save = () => {
+    Navigator(-1);
+  };
+
+  return (
+    <Wallpaper style={{ backgroundImage: `url(${wallpapers[currentIdx]})` }}>
+      <ActionRow>
+        <ActionButton style={{marginRight: "15px"}} onClick={cancel}>
+          취소
+        </ActionButton>
+        <ActionButton highlight onClick={save}>
+          저장
+        </ActionButton>
+      </ActionRow>
+      <ToolBar>
+        <GuideText>게임 내에서 사용할 배경을 골라보세요!</GuideText>
+        <RowWrap>
+          <TransparentButton style={{left: "0"}} onClick={handleLeft}>
+            <ButtonIcon src="/src/assets/icons/ChevronLeft.png" />
+          </TransparentButton>
+          <CarouselWrap ref={carouselRef}>
+            {wallpapers.map((wallpaper, index) => (
+              <Preview key={index} style={{ backgroundImage: `url(${wallpaper})` }} onClick={() => setCurrentIdx(index)} selected={selectedWallpaper === index}>
+                {
+                  index === currentIdx &&
+                  <PreviewFilter>
+                    {/* <ActionButton highlight>구매</ActionButton> */}
+                      <ActionButton disabled={selectedWallpaper === index} onClick={() => setSelectedWallpaper(index)}>
+                        {selectedWallpaper === index ? "장착됨" : "장착"}
+                      </ActionButton>
+                  </PreviewFilter>
+                }
+              </Preview>
+            ))}
+          </CarouselWrap>
+          <TransparentButton style={{right: "0"}} onClick={handleRight}>
+            <ButtonIcon src="/src/assets/icons/ChevronRight.png" />
+          </TransparentButton>
+        </RowWrap>
+      </ToolBar>
+    </Wallpaper>
+  );
+}
+
+export default WallpaperShop;
