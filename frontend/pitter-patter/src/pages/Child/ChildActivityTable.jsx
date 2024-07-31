1c2546a14c3be434be3979807e791d787a868be3
sonbaejun <sonqowns2@naver.com>
2024-07-31 12:19:34 +0900

    Feat : Child 페이지 Style-component 적용 및 메뉴 클릭 시 컴포넌트 전환 기능 추가 [#188]
    Change-Id: Ib79b337af05de15760658a5f0037e99f6db33708

--- frontend/pitter-patter/src/pages/Child/ChildActivityTable.jsx
+++ frontend/pitter-patter/src/pages/Child/ChildActivityTable.jsx
@@ -1,6 +1,6 @@
-import './Child.css';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import styled from 'styled-components';
 
 const data = [
   {
@@ -10,99 +10,111 @@ const data = [
   {
     name: '2024-07-15',
     playtime: 150,
-  },  {
+  }, {
     name: '2024-07-16',
     playtime: 160,
-  },  {
+  }, {
     name: '2024-07-17',
     playtime: 20,
-  },  {
+  }, {
     name: '2024-07-18',
     playtime: 80,
-  },  {
+  }, {
     name: '2024-07-19',
     playtime: 60,
   },
 ];
 
+const LayoutActivityPage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const LayoutGraphList = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const PlaytimeGraph = styled.div`
+  width: 80%;
+  height: 300px; /* 적절한 높이 설정 */
+`;
+
 function ChildActivityTable() {
+  return (
+    <LayoutActivityPage>
+      <LayoutGraphList>
+        <PlaytimeGraph>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="playtime" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+          </ResponsiveContainer>
+        </PlaytimeGraph>
+      </LayoutGraphList>
 
-    return (
-        <div className='layout-activity-page'>
-            <div className='layout-graph-list'> 
-                <div className='playtime-graph'>
-                    <ResponsiveContainer width="100%" height="100%">
-                    <LineChart
-                        width={500}
-                        height={300}
-                        data={data}
-                        margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Line type="monotone" dataKey="playtime" stroke="#8884d8" activeDot={{ r: 8 }} />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
-            </div>
+      <LayoutGraphList>
+        <PlaytimeGraph>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="playtime" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+          </ResponsiveContainer>
+        </PlaytimeGraph>
 
-            <div className='layout-graph-list'>
-                <div className='playtime-graph'>
-                    <ResponsiveContainer width="100%" height="100%">
-                    <LineChart
-                        width={500}
-                        height={300}
-                        data={data}
-                        margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Line type="monotone" dataKey="playtime" stroke="#8884d8" activeDot={{ r: 8 }} />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
-
-                <div className='playtime-graph'>
-                    <ResponsiveContainer width="100%" height="100%">
-                    <LineChart
-                        width={500}
-                        height={300}
-                        data={data}
-                        margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Line type="monotone" dataKey="playtime" stroke="#8884d8" activeDot={{ r: 8 }} />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
-            </div>
-        </div>
-    );
+        <PlaytimeGraph>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="playtime" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+          </ResponsiveContainer>
+        </PlaytimeGraph>
+      </LayoutGraphList>
+    </LayoutActivityPage>
+  );
 }
 
 export default ChildActivityTable;
--- frontend/pitter-patter/src/pages/Child/ChildPage.jsx
+++ frontend/pitter-patter/src/pages/Child/ChildPage.jsx
@@ -1,32 +1,69 @@
-import './Child.css';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation, Routes, Route } from 'react-router-dom';
 import Header from "../LandingPage/Header";
 import ChildActivityTable from './ChildActivityTable';
+import ChildPhysicalInfo from './ChildPhysicalInfo';
+import BMIGraph from './BMIGraph';
+import { Body, ChildBody, ChildBackground, ContextBackground, ContextItem, ChildMenu, ChildMenuItem, Backspace } from './ChildPageStyle';
 
 function ChildPage() {
-    return (
-        <div className="body">
-            <Header />
-            <div className="child-body">
-                <div className="child-background">
-                    <div className='child-menu'>
-                        <Link to="/" className='backspace'>
-                            <img src="src/assets/icons/backArrow.png" alt="" />
-                        </Link>
-                        <div className='child-menu-item' is-active="true">신체 정보 입력하기</div>
-                        <div className='child-menu-item'>활동량 분석표 보기</div>
-                        <div className='child-menu-item'>BMI 분석표 보기</div>
-                    </div>
-                    <div className='context-background'>
-                        <div className='context-item'>
-                            <ChildActivityTable />
-                        </div>
-                    </div>
-                </div>
+  const navigate = useNavigate();
+  const location = useLocation();
 
-            </div>
-        </div>
-    );
+  const getMenuFromPath = (path) => {
+    switch (path) {
+      case '/child/physical-info':
+        return '신체 정보 입력하기';
+      case '/child/activity-table':
+        return '활동량 분석표 보기';
+      case '/child/bmi-graph':
+        return 'BMI 분석표 보기';
+      default:
+        return '신체 정보 입력하기';
+    }
+  };
+
+  const [activeMenu, setActiveMenu] = useState(getMenuFromPath(location.pathname));
+
+  useEffect(() => {
+    const path = activeMenu === '신체 정보 입력하기' ? '/child/physical-info'
+                : activeMenu === '활동량 분석표 보기' ? '/child/activity-table'
+                : '/child/bmi-graph';
+    navigate(path);
+  }, [activeMenu, navigate]);
+
+  return (
+    <Body>
+      <Header />
+      <ChildBody>
+        <ChildBackground>
+          <ChildMenu>
+            <Backspace to="/">
+              <img src="src/assets/icons/backArrow.png" alt="" />
+            </Backspace>
+            <ChildMenuItem $isActive={activeMenu === '신체 정보 입력하기'} onClick={() => setActiveMenu('신체 정보 입력하기')}>
+              신체 정보 입력하기
+            </ChildMenuItem>
+            <ChildMenuItem $isActive={activeMenu === '활동량 분석표 보기'} onClick={() => setActiveMenu('활동량 분석표 보기')}>
+              활동량 분석표 보기
+            </ChildMenuItem>
+            <ChildMenuItem $isActive={activeMenu === 'BMI 분석표 보기'} onClick={() => setActiveMenu('BMI 분석표 보기')}>
+              BMI 분석표 보기
+            </ChildMenuItem>
+          </ChildMenu>
+          <ContextBackground>
+            <ContextItem>
+              <Routes>
+                <Route path="physical-info" element={<ChildPhysicalInfo />} />
+                <Route path="activity-table" element={<ChildActivityTable />} />
+                <Route path="bmi-graph" element={<BMIGraph />} />
+              </Routes>
+            </ContextItem>
+          </ContextBackground>
+        </ChildBackground>
+      </ChildBody>
+    </Body>
+  );
 }
 
 export default ChildPage;
