0641d3bcf7b3a3fb39f3256ad414e240fd6588f8
김상범 <picel@picel.net>
2024-07-31 09:16:27 +0900

    Feat : 상점 페이지 작성 (1/3) [#223]
    - 상점 선택 페이지 작성
    - Link to 설정
    
    Change-Id: Icf7a412505320d4da6d3ec6ccff454c87d598038

--- frontend/pitter-patter/src/pages/Shop/Shop.jsx
+++ frontend/pitter-patter/src/pages/Shop/Shop.jsx
@@ -1,21 +1,27 @@
 import VS from "../../assets/img/Game/vs.png";
 import { Wallpaper, BoxWrap, ImageBox, GameImage, ItemName } from "./ShopStyle";
 
+import { Link } from "react-router-dom";
+
 function Shop() {
   return (
     <Wallpaper>
-      <BoxWrap bgColor="#ffe65c">
-        <ImageBox shadow="4px 4px 27.5px 0px #c0a93e inset">
-          <GameImage src={VS} alt="VS" />
-        </ImageBox>
-        <ItemName color="#f9810a">네 컷 프레임 상점</ItemName>
-      </BoxWrap>
-      <BoxWrap bgColor="#a4e6f3">
-        <ImageBox shadow="4px 4px 27.5px 0px #6ca0ab inset">
-          <GameImage src={VS} alt="VS" />
-        </ImageBox>
-        <ItemName color="#17b1f3">게임 배경 상점</ItemName>
-      </BoxWrap>
+      <Link to="/shop/snapshot">
+        <BoxWrap bgColor="#ffe65c">
+          <ImageBox shadow="4px 4px 27.5px 0px #c0a93e inset">
+            <GameImage src={VS} alt="VS" />
+          </ImageBox>
+          <ItemName color="#f9810a">네 컷 프레임 상점</ItemName>
+        </BoxWrap>
+      </Link>
+      <Link to="/shop/wallpaper">
+        <BoxWrap bgColor="#a4e6f3">
+          <ImageBox shadow="4px 4px 27.5px 0px #6ca0ab inset">
+            <GameImage src={VS} alt="VS" />
+          </ImageBox>
+          <ItemName color="#17b1f3">게임 배경 상점</ItemName>
+        </BoxWrap>
+      </Link>
     </Wallpaper>
   );
 }
