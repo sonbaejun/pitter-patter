f3abadee560e1ab14a052b0264a5047af848225f
이승민 <lsm5631@naver.com>
2024-08-01 10:13:01 +0900

    Feat : error페이지 제작 [#199]
    Change-Id: I36e97681a97e80482710217bddcce5affb06cb84

--- frontend/pitter-patter/src/App.jsx
+++ frontend/pitter-patter/src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from "./pages/LandingPage/LandingPage"
 import ChildPage from "./pages/Child/ChildPage"
 import Login from "./pages/User/Login"
 import Attendance from "./pages/ETC/Attendance/Attendance"
+import Error from "./pages/Error"
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
           {/* ETC */}
           <Route path="/attendance" element={<Attendance />} />
 
+          {/* ERROR */}
+          <Route path="/error" element={<Error />} />
         </Routes>
       </div>
     </BrowserRouter>
