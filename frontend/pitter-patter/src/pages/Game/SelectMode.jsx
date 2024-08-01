930d4fe91d571383ca212022675cc79c02b16483
김상범 <picel@picel.net>
2024-08-01 10:06:20 +0900

    Feat : 게임 선택 화면 하이퍼링크 설정
    Change-Id: I7ad7a4b735e6268ec8c5cc2d99dbae5cf5312fda

--- frontend/pitter-patter/src/pages/Game/SelectMode.jsx
+++ frontend/pitter-patter/src/pages/Game/SelectMode.jsx
@@ -1,21 +1,26 @@
+import { Link } from "react-router-dom";
 import VS from "../../assets/img/Game/vs.png";
 import { MainWrap, BoxWrap, ImageBox, GameImage, ItemName } from "./SelectModeStyle";
 
 function SelectMode() {
   return (
     <MainWrap>
-      <BoxWrap bgColor="var(--logo-pink-color)">
-        <ImageBox shadow="4px 4px 27.5px 0px #933557 inset">
-          <GameImage src={VS} alt="VS" />
-        </ImageBox>
-        <ItemName color="#FFD8DF">게임 시작하기</ItemName>
-      </BoxWrap>
-      <BoxWrap bgColor="var(--box-green-color)">
-        <ImageBox shadow="4px 4px 27.5px 0px #629D1B inset">
-          <GameImage src={VS} alt="VS" />
-        </ImageBox>
-        <ItemName color="#629D1B">인생 네 컷 찍기</ItemName>
-      </BoxWrap>
+      <Link to={"/game"}>
+        <BoxWrap bgColor="var(--logo-pink-color)">
+          <ImageBox shadow="4px 4px 27.5px 0px #933557 inset">
+            <GameImage src={VS} alt="VS" />
+          </ImageBox>
+          <ItemName color="#FFD8DF">게임 시작하기</ItemName>
+        </BoxWrap>
+      </Link>
+      <Link to={"/game/snapshot"}>
+        <BoxWrap bgColor="var(--box-green-color)">
+          <ImageBox shadow="4px 4px 27.5px 0px #629D1B inset">
+            <GameImage src={VS} alt="VS" />
+          </ImageBox>
+          <ItemName color="#629D1B">인생 네 컷 찍기</ItemName>
+        </BoxWrap>
+      </Link>
     </MainWrap>
   );
 }
