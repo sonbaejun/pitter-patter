9b33824b22e7cf7d6f8952ea86f4b4662f70c883
윤예리 <yoonr721@gmail.com>
2024-08-06 14:21:21 +0900

    Feat : 게임 api추가 [#248]
    Change-Id: I213f09f3a4e861e50a4b33981ce72a42a29c3a62
    
    Former-commit-id: d83dcf2752bb22c967708e4d131f29b400540048

--- frontend/pitter-patter/src/pages/Game/GamePage.jsx
+++ frontend/pitter-patter/src/pages/Game/GamePage.jsx
@@ -4,6 +4,7 @@ import Header from "../LandingPage/Header";
 import IsReady from "../Game/IsReady";
 import WebcamTestPage from "../Game/WebcamTestPage";
 import Unity from "./Unity";
+import { getWallpaper } from "./gameApi.js";
 
 function GamePage() {
   const [modalOpen, setModalOpen] = useState(true);
@@ -17,8 +18,11 @@ function GamePage() {
     setModalOpen(false);
   };
 
+  const childId = 5;
+
   useEffect(() => {
     setModalOpen(true); // 페이지 접근 시 모달 창 열기
+    getWallpaper(childId) // childId를 넣으면 그 아이가 착용하고 있는 벽의 이미지 주소가 return됨
   }, []);
 
   return (
