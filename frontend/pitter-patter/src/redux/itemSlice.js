dde049052e2ccf0abfcb406b5cc7a65367047e6b
김상범 <picel@picel.net>
2024-08-13 10:24:47 +0900

    Hotfix : 네컷 프레임 표시 안 되던 문제 해결
    Change-Id: Ie24018d60ae901feaa47354bae50bd4558df97c5
    
    Former-commit-id: 5dd93b5f5732105c6eba23ee6f9ce2df3659cb47

--- frontend/pitter-patter/src/redux/itemSlice.js
+++ frontend/pitter-patter/src/redux/itemSlice.js
@@ -1,64 +1,63 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { assetsApi } from '../apiService';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { assetsApi } from "../apiService";
 
 // 착용 아이템 정보 GET
-export const getItem = createAsyncThunk(
-    'item/getItem',
-    async (_, thunkAPI) => {
-        try {
-            const state = thunkAPI.getState();
-            const childId = state.child.id; 
-            const token = state.token.refreshToken;
-            const response = await assetsApi.get(`/item-property/${childId}/on`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            console.log('API Response:', response.data); // 응답 데이터 출력
-            return response.data;
-        } catch (error) {
-            console.log('API Error:', error.response.data); // 에러 데이터 출력
-            return thunkAPI.rejectWithValue(error.response.data);
-        }
-    }
-);
+export const getItem = createAsyncThunk("item/getItem", async (_, thunkAPI) => {
+  try {
+    const state = thunkAPI.getState();
+    const childId = state.child.id;
+    const token = state.token.refreshToken;
+    const response = await assetsApi.get(`/item-property/${childId}/on`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log("API Response:", response.data); // 응답 데이터 출력
+    return response.data;
+  } catch (error) {
+    console.log("API Error:", error.response.data); // 에러 데이터 출력
+    return thunkAPI.rejectWithValue(error.response.data);
+  }
+});
 
 export const itemSlice = createSlice({
-    name: 'item',
-    initialState: {
-        frameItem: "https://ssafy-common.b-cdn.net/background_1.png",
-        backgroundItem: 1,
-        status: 'idle',
-        error: null,
+  name: "item",
+  initialState: {
+    frameItem: 1,
+    backgroundItem: 1,
+    status: "idle",
+    error: null,
+  },
+  reducers: {
+    clearItem: (state) => {
+      state.frameItem = 1;
+      state.backgroundItem = 1;
+      state.status = "idle";
+      state.error = null;
     },
-    reducers: {
-        clearItem: (state) => {
-            state.frameItem = "https://ssafy-common.b-cdn.net/background_1.png";
-            state.backgroundItem = 1;
-            state.status = 'idle';
-            state.error = null;
-        },
-        setItem: (state, action) => {
-            state.frameItem = action.payload[0] ? action.payload[0].photo : "https://ssafy-common.b-cdn.net/background_1.png";
-            state.backgroundItem = action.payload[1] ? action.payload[1].id : 1;
-        },
+    setItem: (state, action) => {
+      state.frameItem = action.payload[0] ? action.payload[0].photo : 1;
+      state.backgroundItem = action.payload[1] ? action.payload[1].id : 1;
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(getItem.pending, (state) => {
-                state.status = 'loading';
-            })
-            .addCase(getItem.fulfilled, (state, action) => {
-                state.status = 'succeeded';
-                console.log(action.payload);
-                state.frameItem = action.payload[0] ? action.payload[0].photo : "https://ssafy-common.b-cdn.net/background_1.png";
-                state.backgroundItem = action.payload[1] ? action.payload[1].id : 1;
-            })
-            .addCase(getItem.rejected, (state, action) => {
-                state.status = 'failed';
-                state.error = action.payload ? action.payload : action.error.message;
-            });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getItem.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getItem.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        console.log(action.payload);
+        state.frameItem = action.payload[0]
+          ? action.payload[0].photo
+          : "https://ssafy-common.b-cdn.net/background_1.png";
+        state.backgroundItem = action.payload[1] ? action.payload[1].id : 1;
+      })
+      .addCase(getItem.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload ? action.payload : action.error.message;
+      });
+  },
 });
 
 // 액션 생성자(export 하기)
