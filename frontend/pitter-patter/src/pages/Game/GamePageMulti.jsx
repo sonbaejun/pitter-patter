712e2ebdb2d94a232aaba27684c6de5618d918fb
김상범 <picel@picel.net>
2024-08-14 14:08:23 +0900

    Feat : 멀티 플레이 게임 결과 화면 추가
    Change-Id: I8fcfbe69b3187a4a057f842895d3fda8616a69bc
    
    Former-commit-id: 409036405d78a120732c09f64ec1059ab0f0babe

--- frontend/pitter-patter/src/pages/Game/GamePageMulti.jsx
+++ frontend/pitter-patter/src/pages/Game/GamePageMulti.jsx
@@ -1,18 +1,17 @@
 import { MainWrap, GraphWrap, BarWrap, Bar, NameWrap, Name } from "./GamePageMultiStyle";
 import Header from "../LandingPage/Header";
 import MultiSelectModal from "./MultiSelectModal";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { fetchRoomId, initializeSocket, sendMessage } from "./multiApi";
 import { useNavigate } from "react-router-dom";
 import Unity from "./Unity";
-import AttModal from "./AttModal";
 import { useSelector } from "react-redux";
 import { getWallpaper } from "./gameApi.js";
-import { assetsApi, gameApi } from "../../apiService.js";
 import WebcamTestPage from "../Game/WebcamTestPage";
 import Loader from "../Components/loader.jsx";
 import IsReady from "../Game/IsReady";
 import DodgeAlertModal from "./DodgeAlertModal.jsx";
+import MultiResultModal from "./MultiResultModal.jsx";
 
 function GamePageMulti() {
   const [socket, setSocket] = useState(null);
@@ -22,13 +21,12 @@ function GamePageMulti() {
   const [isStarted, setIsStarted] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
   const [testCompleted, setTestCompleted] = useState(false);
-  const [attModalOpen, setAttModalOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false); // 로딩 상태 추가
   const [score, setScore] = useState(0);
   const [rivalScore, setRivalScore] = useState(0);
+  const [resultModalOpen, setResultModalOpen] = useState(false);
   const [dodgeAlertModalOpen, setDodgeAlertModalOpen] = useState(false);
   
-  const token = useSelector((state) => state.token.accessToken);
   const childId = useSelector((state) => state.child.id);
 
   const navigate = useNavigate();
@@ -90,58 +88,11 @@ function GamePageMulti() {
     }
   }, [score]);
 
-  const openAttModal = async () => {
-    try {
-      const firstResponse = await gameApi.post(`/${childId}`, {
-        score: 0,
-        playtime: 0,
-        burnedCalorie: 0,
-      }, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      console.log('첫 번째 요청 성공:', firstResponse.data);
-
-      if (firstResponse.data.result === true) {
-        const secondResponse = await assetsApi.patch('/point', {
-          amount: 12,
-          source: '게임 플레이 보상',
-          childId: childId,
-        }, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        console.log('두 번째 포인트 지급 성공:', secondResponse.data);      
-        setAttModalOpen(true);
-      } else {
-        const secondResponse = await assetsApi.patch('/point', {
-          amount: 2,
-          source: '게임 플레이 보상',
-          childId: childId,
-        }, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        console.log('두 번째 포인트 지급 성공:', secondResponse.data);
-      }
-
-    } catch (error) {
-      console.error('요청 실패:', error);
-    }
-  };
-
-  const closeAttModal = () => {
-    setAttModalOpen(false);
+  const onGameEnd = () => {
+    setResultModalOpen(true);
   };
 
   useEffect(() => {
-    openAttModal();
     getWallpaper(childId); // childId를 넣으면 그 아이가 착용하고 있는 벽의 이미지 주소가 return됨
   }, [childId]);
 
@@ -151,7 +102,7 @@ function GamePageMulti() {
       {testCompleted ? (
         <div>
           {isLoading && <Loader />}
-          <Unity onGameEnd={openAttModal} isLoading={isLoading} setIsLoading={setIsLoading} score={score} setScore={setScore} />
+          <Unity onGameEnd={onGameEnd} isLoading={isLoading} setIsLoading={setIsLoading} score={score} setScore={setScore} />
         </div>
       ) : (
         <WebcamTestPage onTestComplete={handleTestComplete} />
@@ -170,7 +121,7 @@ function GamePageMulti() {
         <MultiSelectModal onJoinRoom={handleJoinRoom} onCreateRoom={handleCreateRoom} isWaiting={isWaiting} roomId={roomId} />
       }
       {modalOpen && <IsReady onClose={closeModal} />}
-      {attModalOpen && <AttModal onClose={closeAttModal} />}
+      {resultModalOpen && <MultiResultModal onClose={() => navigate("/game/select-mode")} myScore={score} rivalScore={rivalScore} />}
       {dodgeAlertModalOpen && <DodgeAlertModal onClose={() => navigate("/game/select-mode")} />}
     </MainWrap>
   );
---
+++ frontend/pitter-patter/src/pages/Game/MultiResultModal.jsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+import Win from "/src/assets/img/Game/win.png";
+import Lose from "/src/assets/img/Game/lose.png";
+import Draw from "/src/assets/img/Game/draw.png";
+
+const ModalOverlay = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+`;
+
+const ModalContent = styled.div`
+  background: white;
+  padding: 5vh 10vw;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CloseButton = styled.button`
+  background-color: var(--box-yellow-color);
+  border-radius: 21px;
+  width: fit-content;
+  padding: .3rem 1rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  box-shadow: 0px 5px 0px 0px var(--logo-yellow-color);
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    transform: translateY(1px);
+    box-shadow: 0px 4px 0px 0px var(--logo-yellow-color);
+  }
+
+  &:active {
+    transform: translateY(5px);
+    box-shadow: 0px 0px 0px 0px var(--logo-yellow-color);
+  }
+`;
+
+const ModalImg = styled.img`
+  width: 15vw; /* 필요에 따라 크기를 조절하세요 */
+  margin-bottom: 1rem;
+`;
+
+function MultiResultModal({ onClose, myScore, rivalScore }) {
+
+  return (
+    <ModalOverlay>
+      <ModalContent>
+        <ModalImg src={myScore > rivalScore ? Win : myScore < rivalScore ? Lose : Draw} alt="Result" />
+        <h2>{myScore > rivalScore ? '승리!' : myScore < rivalScore ? '패배...' : '무승부!'}</h2>
+        <div style={{ display: 'flex', width: '100%', justifyContent: 'center', gap: '1rem', marginTop: '1rem' }}>
+          <CloseButton onClick={onClose}>돌아가기</CloseButton>
+        </div>
+      </ModalContent>
+    </ModalOverlay>
+  );
+}
+
+export default MultiResultModal;
