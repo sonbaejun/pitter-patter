2667704c46961de978273f1064b90decf264032d
김상범 <picel@picel.net>
2024-08-14 09:49:56 +0900

    feat: 카운트다운 추가, 소리 관련 수정
    Change-Id: I7196a484fad2e86305a0fd8f1a42077190b96d0f
    
    Former-commit-id: e00a6a8f567396f8b5c9c95254486af2b5ac4e02

--- frontend/pitter-patter/src/pages/Game/Snapshot/Snapshot.jsx
+++ frontend/pitter-patter/src/pages/Game/Snapshot/Snapshot.jsx
@@ -16,6 +16,7 @@ import {
   Modal,
   ModalContent,
   ViewFinder,
+  Countdown,
 } from "./SnapshotStyle";
 import AddImageIcon from "/src/assets/icons/AddImage.png";
 import EG1 from "/src/assets/img/Snapshot/eg1.png";
@@ -27,24 +28,23 @@ import Beep from "/src/assets/sound/beep.mp3";
 import Shutter from "/src/assets/sound/shutter.mp3";
 
 function Snapshot() {
-  // const frameRef = useRef(null);
   const [imageList, setImageList] = useState([null, null, null, null]);
   const navigate = useNavigate();
   const BlankRefs = useRef([]);
   const [isFilled, setIsFilled] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalVideoStream, setModalVideoStream] = useState(null);
+  const [countdown, setCountdown] = useState(3); // countdown 상태 추가
+  const videoRef = useRef(null); // 비디오 ref 추가
 
   const FrameImage = "https://ssafy-common.b-cdn.net/frame_" + useSelector((state) => state.item.frameItem) + ".png";
 
   function getImage(index) {
-    const targetRef = BlankRefs.current[index];
 
     if (imageList[index]) {
       setImageList(imageList.map((img, i) => (i === index ? null : img)));
     }
 
-    const video = document.createElement("video");
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
 
@@ -54,84 +54,68 @@ function Snapshot() {
     canvas.height = canvasHeight;
     canvas.style.objectFit = "cover";
 
-    video.width = canvasWidth;
-    video.height = canvasHeight;
-
-    video.style.position = "absolute";
-    video.style.top = "0";
-    video.style.left = "0";
-    video.style.transform = "scaleX(-1)";
-
     let stream = null;
 
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
       .then((mediaStream) => {
         stream = mediaStream;
-        video.srcObject = mediaStream;
-        video.play();
-        targetRef.appendChild(video);
         setModalVideoStream(mediaStream);
         setIsModalOpen(true);
+        setCountdown(3); // 초기 카운트다운 설정
+
+        const beep = new Audio(Beep);
+        beep.play();
+
+        const interval = setInterval(() => {
+          setCountdown((prevCountdown) => {
+            if (prevCountdown === 1) {
+              const shutter = new Audio(Shutter);
+              shutter.play();
+              clearInterval(interval);
+
+              const video = videoRef.current;
+              const videoWidth = video.videoWidth;
+              const videoHeight = video.videoHeight;
+              const aspectRatio = canvasWidth / canvasHeight;
+              let cropWidth, cropHeight;
+
+              if (videoWidth / videoHeight > aspectRatio) {
+                cropHeight = videoHeight;
+                cropWidth = videoHeight * aspectRatio;
+              } else {
+                cropWidth = videoWidth;
+                cropHeight = videoWidth / aspectRatio;
+              }
+
+              const cropX = (videoWidth - cropWidth) / 2;
+              const cropY = (videoHeight - cropHeight) / 2;
+
+              ctx.translate(canvasWidth, 0);
+              ctx.scale(-1, 1);
+
+              ctx.drawImage(video, cropX, cropY, cropWidth, cropHeight, 0, 0, canvasWidth, canvasHeight);
+              const image = canvas.toDataURL("image/png");
+              setImageList(imageList.map((img, i) => (i === index ? image : img)));
+
+              let tracks = stream.getTracks();
+              tracks.forEach(function (track) {
+                track.stop();
+              });
+
+              video.srcObject = null;
+              setIsModalOpen(false);
+              setModalVideoStream(null);
+            } else {
+              const beep = new Audio(Beep);
+              beep.play();
+            }
+            return prevCountdown - 1;
+          });
+        }, 1000);
       })
       .catch((err) => {
         console.log(err);
       });
-
-    let countdown = 3;
-
-    const beep = new Audio(Beep);
-    beep.volume = 0.5;
-    beep.play();
-
-    const interval = setInterval(() => {
-      countdown -= 1;
-
-      if (countdown === 0) {
-        const shutter = new Audio(Shutter);
-        shutter.play();
-      } else {
-        const beep = new Audio(Beep);
-        beep.volume = 0.5;
-        beep.play();
-      }
-
-      if (countdown === 0) {
-        clearInterval(interval);
-
-        const videoWidth = video.videoWidth;
-        const videoHeight = video.videoHeight;
-        const aspectRatio = canvasWidth / canvasHeight;
-        let cropWidth, cropHeight;
-
-        if (videoWidth / videoHeight > aspectRatio) {
-          cropHeight = videoHeight;
-          cropWidth = videoHeight * aspectRatio;
-        } else {
-          cropWidth = videoWidth;
-          cropHeight = videoWidth / aspectRatio;
-        }
-
-        const cropX = (videoWidth - cropWidth) / 2;
-        const cropY = (videoHeight - cropHeight) / 2;
-
-        ctx.translate(canvasWidth, 0);
-        ctx.scale(-1, 1);
-
-        ctx.drawImage(video, cropX, cropY, cropWidth, cropHeight, 0, 0, canvasWidth, canvasHeight);
-        const image = canvas.toDataURL("image/png");
-        setImageList(imageList.map((img, i) => (i === index ? image : img)));
-        video.remove();
-
-        let tracks = stream.getTracks();
-        tracks.forEach(function(track) {
-          track.stop();
-        });
-
-        video.srcObject = null;
-        setIsModalOpen(false);
-        setModalVideoStream(null);
-      }
-    }, 1000);
   }
 
   useEffect(() => {
@@ -139,6 +123,12 @@ function Snapshot() {
       setIsFilled(true);
     }
   }, [imageList, navigate]);
+
+  useEffect(() => {
+    if (videoRef.current && modalVideoStream) {
+      videoRef.current.srcObject = modalVideoStream;
+    }
+  }, [modalVideoStream]);
 
   return (
     <MainWrap>
@@ -168,43 +158,46 @@ function Snapshot() {
             <BlankRow>
               <Blank onClick={() => getImage(0)} active ref={(el) => (BlankRefs.current[0] = el)}>
                 {(imageList[0] && <UserImg src={imageList[0]} alt="snapshot" />) || (
-                    <AddImg src={AddImageIcon} alt="add" />
+                  <AddImg src={AddImageIcon} alt="add" />
                 )}
               </Blank>
               <Blank onClick={() => getImage(1)} active ref={(el) => (BlankRefs.current[1] = el)}>
                 {(imageList[1] && <UserImg src={imageList[1]} alt="snapshot" />) || (
-                    <AddImg src={AddImageIcon} alt="add" />
+                  <AddImg src={AddImageIcon} alt="add" />
                 )}
               </Blank>
             </BlankRow>
             <BlankRow>
               <Blank onClick={() => getImage(2)} active ref={(el) => (BlankRefs.current[2] = el)}>
                 {(imageList[2] && <UserImg src={imageList[2]} alt="snapshot" />) || (
-                    <AddImg src={AddImageIcon} alt="add" />
+                  <AddImg src={AddImageIcon} alt="add" />
                 )}
               </Blank>
               <Blank onClick={() => getImage(3)} active ref={(el) => (BlankRefs.current[3] = el)}>
                 {(imageList[3] && <UserImg src={imageList[3]} alt="snapshot" />) || (
-                    <AddImg src={AddImageIcon} alt="add" />
+                  <AddImg src={AddImageIcon} alt="add" />
                 )}
               </Blank>
             </BlankRow>
           </Frame>
         </CenterRow>
       </CenterColumn>
-      {isFilled && <GoResultButton onClick={() => navigate("./result", { state: { imageList } })}>
-        결과 확인하기
-      </GoResultButton>}
+      {isFilled && (
+        <GoResultButton onClick={() => navigate("./result", { state: { imageList } })}>
+          결과 확인하기
+        </GoResultButton>
+      )}
       {isModalOpen && (
-        <Modal>
-          <ModalContent>
-            <ViewFinder autoPlay muted style={{ transform: 'scaleX(-1)' }} ref={videoRef => {
-              if (videoRef) {
-                videoRef.srcObject = modalVideoStream;
-              }
-            }}></ViewFinder>
-          </ModalContent>
-        </Modal>
+        <>
+          <Modal>
+            <ModalContent>
+              <ViewFinder autoPlay muted ref={videoRef} style={{ transform: "scaleX(-1)" }}></ViewFinder>
+            </ModalContent>
+          </Modal>
+          <Countdown>
+            <span>{countdown}</span>
+          </Countdown>
+        </>
       )}
     </MainWrap>
   );
